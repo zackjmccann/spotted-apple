a39c3e114c3e0f603c68c2127b4831dc4ad38b65
Zachary J McCann <zackjmccan@gmail.com>
2024-06-29 23:45:03 -0400

    adding helper file with aux functions

---
+++ src/helpers.py
@@ -0,0 +1,36 @@
+import os
+import re
+import streamlit as st
+
+def handle_app_request():
+    if validate_email_input():
+        if check_if_user_has_app_access():
+            st.session_state['app_access_request'] = 'granted'
+        else:
+            st.session_state['app_access_request'] = 'pending'
+    else:
+        st.session_state['app_access_request'] = 'invalid'
+
+
+def check_if_user_has_app_access():
+    #TODO: Add check >> store users with development access to redis and validate
+    if st.session_state['user_email'] == os.getenv('SPOTIFY_ADMIN'):
+        return True
+    else:
+        return False
+
+
+def validate_email_input():
+  regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+  if st.session_state['user_email'] == '':
+    return False
+
+  try:
+    if re.fullmatch(regex, st.session_state['user_email']):
+        return True
+    else:
+        return False
+
+  except TypeError:
+        return False
