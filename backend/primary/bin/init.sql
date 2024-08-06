889950d0849e6429080327ae3f39612389983382
Zachary J McCann <zackjmccan@gmail.com>
2024-08-05 23:36:49 -0400

    adding init sql ddl

--- backend/primary/bin/init.sql
+++ backend/primary/bin/init.sql
@@ -4,3 +4,16 @@ CREATE TABLE IF NOT EXISTS users (
   password TEXT NOT NULL,
   created  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS access_tokens (
+  access_token_id VARCHAR PRIMARY KEY,
+  account         VARCHAR,
+  user_id         BIGINT references users(user_id),
+  access_token    VARCHAR,
+  token_type      VARCHAR,
+  scope           VARCHAR,
+  expiration_time TIMESTAMPTZ NOT NULL,
+  refresh_token   VARCHAR,
+  created         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  modified        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
