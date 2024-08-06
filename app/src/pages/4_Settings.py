fbc4a6faba7f07a7e5b1a852a1c06fc8b95effea
Zachary J McCann <zackjmccan@gmail.com>
2024-08-05 23:36:18 -0400

    adding logic to token and authorization flow

--- app/src/pages/4_Settings.py
+++ app/src/pages/4_Settings.py
@@ -1,40 +1,41 @@
 import streamlit as st
 from navigation import make_sidebar
 from auth.spotify import SpotifyOAuth
-from auth.apple_music import authorize_apple_music
+from auth.spotted_apple import get_access_token
 
 make_sidebar()
 if not st.session_state.get('is_logged_in', False):
     st.switch_page("Spotted_Apple.py")
 
-spotify = SpotifyOAuth(user_id=st.session_state['user_id'])
+spotify = SpotifyOAuth(state=st.session_state['user_id'])
 
-if 'spotify_auth' not in st.session_state:
-    st.session_state['spotify_auth'] = False
-if 'apple_music_auth' not in st.session_state:
-    st.session_state['apple_music_auth'] = False
+if 'spotify_access_token' not in st.session_state:
+    st.session_state['spotify_access_token'] = get_access_token('spotify', st.session_state['user_id'])
 
-st.title('Spotted Apple :apple::snake:')
+st.title('Settings :gear:')
 
 st.header('Account Authorizations')
 spotify_widget, apple_widget = st.columns(2)
 
 with spotify_widget:
-    # TODO: Write the user_id to local storage, that way when the authorization occurs, you can reference
-    # TODO: the logged in user in the auth_landing page, associate the token with the account on the
-    # TODO: backend, then refresh the session with the authoriation token.
-
     st.subheader('Spotify')
-    if not st.session_state['spotify_auth']:
+    if not st.session_state['spotify_access_token']:
         st.button(
             label='Authorize',
-            key='spotify_auth_2',
             on_click=spotify.authorize)
+    else:
+        st.info('You have authorized access to Spotify.')
+        st.button(
+            label='Refresh Token',
+            on_click=spotify.refresh_access_token)
 
 with apple_widget:
     st.subheader('Apple Music')
-    if not st.session_state['apple_music_auth']:
-        st.button(
-            label='Authorize',
-            key='apple_music_auth_2',
-            on_click=authorize_apple_music)
+    st.button(
+        label='Authorize',
+        key='apple_music_auth')
+
+st.write('##')
+st.write('##')
+if st.button('Refresh Authorizations'):
+    st.rerun()
--- app/src/pages/auth_landing.py
+++ app/src/pages/auth_landing.py
@@ -1,17 +1,19 @@
 import streamlit as st
 from navigation import make_sidebar
 from auth.spotify import SpotifyOAuth
-from auth.spotted_apple import get_access_token
+from auth.spotted_apple import save_access_token
 
 make_sidebar()
 
 st.session_state['user_id'] = st.query_params["state"]
 st.session_state['spotify_auth_code'] = st.query_params['code']
 
-access_token_info = spotify.get_access_token(st.session_state['spotify_auth_code'])
+spotify = SpotifyOAuth(state=st.session_state['user_id'])
+access_token_info = spotify.get_access_token_info(st.session_state['spotify_auth_code'])
+access_token = save_access_token('spotify', access_token_info)
 
-if access_token_info:
-    pass # TODO: Call spotted_apple function to store token data
-
-st.header('Authorized with Spotify!')
-st.write('You can close this window.')
+if access_token:
+    st.success('Authorized with Spotify!')
+    st.write('You can close this window.')
+else:
+    st.error('Unable to authorize Spotify. Please try again.')
