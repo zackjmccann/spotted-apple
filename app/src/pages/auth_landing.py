05877f25209330c5d7a611ba0b665a4e8062cfdb
Zachary J McCann <zackjmccan@gmail.com>
2024-08-05 23:54:27 -0400

    handle rogue navigation to the auth landing page

--- app/src/pages/auth_landing.py
+++ app/src/pages/auth_landing.py
@@ -1,19 +1,22 @@
 import streamlit as st
 from navigation import make_sidebar
+from auth.oauth import AuthenticationError
 from auth.spotify import SpotifyOAuth
 from auth.spotted_apple import save_access_token
 
 make_sidebar()
 
-st.session_state['user_id'] = st.query_params["state"]
-st.session_state['spotify_auth_code'] = st.query_params['code']
+if st.query_params:
+    st.session_state['user_id'] = st.query_params["state"]
+    st.session_state['spotify_auth_code'] = st.query_params['code']
 
-spotify = SpotifyOAuth(state=st.session_state['user_id'])
-access_token_info = spotify.get_access_token_info(st.session_state['spotify_auth_code'])
-access_token = save_access_token('spotify', access_token_info)
+try:
+    spotify = SpotifyOAuth(state=st.session_state['user_id'])
+    access_token_info = spotify.get_access_token_info(st.session_state['spotify_auth_code'])
+    access_token = save_access_token('spotify', access_token_info)
+    if access_token:
+        st.success('Authorized with Spotify!')
+        st.write('You can close this window.')
 
-if access_token:
-    st.success('Authorized with Spotify!')
-    st.write('You can close this window.')
-else:
-    st.error('Unable to authorize Spotify. Please try again.')
+except (AuthenticationError, KeyError):
+    st.warning('The authorization information on this page is stale. Please revisit your original session and try agin.')
