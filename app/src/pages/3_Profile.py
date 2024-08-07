dcb82ffacbeb13a1794d0c05b5d8f4bcbe03fa22
Zachary J McCann <zackjmccan@gmail.com>
2024-08-07 17:52:41 -0400

    removing python frontend

--- app/src/pages/3_Profile.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from navigation import make_sidebar
-
-make_sidebar()
-if not st.session_state.get('is_logged_in', False):
-    st.switch_page("Spotted_Apple.py")
-
-st.title('Spotted Apple :apple::snake:')
-st.session_state
