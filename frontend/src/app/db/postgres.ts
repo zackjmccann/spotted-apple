ba3910223b7357f046fc47b4ecce288bcb6ad081
Zachary J McCann <zackjmccan@gmail.com>
2024-08-11 15:21:33 -0400

    adding user management methods

--- frontend/src/app/db/postgres.ts
+++ frontend/src/app/db/postgres.ts
@@ -1,4 +1,4 @@
-import postgres from 'postgres'
+import postgres, { PostgresError } from 'postgres'
 
 let db = postgres('postgres://', {
     username: process.env.POSTGRES_USER ?? 'postgres',
@@ -9,23 +9,57 @@ let db = postgres('postgres://', {
     max: 10
   });
 
+export interface UserData {
+  // TODO: Change types
+  user_id: string,
+  email: string,
+  created: string
+};
 
 export async function connect() {
   return await db
 }
 
-export async function insert() {
-  let test_email = 'unit_test_user@email.com'
-  let test_password = 'unit_test_password'
+export async function insert_user(email:string, password: string) {
+  try {
+    let insert_result = await db`
+    INSERT INTO users (email, password)
+    VALUES (${email}, ${password})
+    RETURNING users.user_id
+    `
+    return insert_result[0].user_id
 
-  let insert_result = await db`
-  INSERT INTO users (email, password)
-  VALUES (${test_email}, ${test_password})
-  RETURNING users.user_id
+  } catch (err) {
+    if(err.name == 'PostgresError' &&
+      err.message == `duplicate key value violates unique constraint "users_email_key"`) {
+      console.log(`User "${email}" already exists.`);
+      return -1;
+    };
+
+  };
+};
+
+export async function delete_user(user_id: number) {
+  try {
+    let insert_result = await db`
+    DELETE FROM users 
+    WHERE users.user_id = ${user_id}
+    RETURNING users.user_id;
+    `
+    return insert_result[0].user_id
+
+  } catch (err) {
+      return err;
+  };
+};
+
+export async function get_user_data_by_email(email:string): Promise<UserData> {
+  let user_result = await db`
+  SELECT user_id, email, created
+  FROM users 
+    WHERE email = ${email};
   `
-  db.end()
-  return insert_result[0].user_id
-
+  return <UserData> user_result[0]
 };
 
 export * as db from './postgres';
