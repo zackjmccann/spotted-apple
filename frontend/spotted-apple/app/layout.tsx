05428ae4ce0fcaeba4d8ee396be5367d1d0289e5
Zachary J McCann <zackjmccan@gmail.com>
2024-09-14 13:29:27 -0400

    change imports for new ui structure

--- frontend/spotted-apple/app/layout.tsx
+++ frontend/spotted-apple/app/layout.tsx
@@ -1,8 +1,6 @@
+import '@/app/ui/global.css';
+import { inter } from "@/app/ui/fonts";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Spotted Apple",
