31379ea06a0873ccaa8e23c77e5ac98cd09154de
Zachary J McCann <zackjmccan@gmail.com>
2024-09-15 08:40:26 -0400

    moving unique keys to parent element

--- frontend/spotted-apple/app/ui/home-links.tsx
+++ frontend/spotted-apple/app/ui/home-links.tsx
@@ -4,21 +4,25 @@ import Link from 'next/link';
 
 const links = [
     { 
+        key: 'create-account',
         name: 'Create Account',
         href: '/signup',
         description: 'Get started with Spotted Apple and share music faster.'
-    },
-    { 
+      },
+      {
+        key: 'login',
         name: 'Login',
         href: '/',
         description: 'Already a user? Log in to access your account.'
-    },
-    { 
+      },
+      { 
+        key: 'how-it-works',
         name: 'How it Works',
         href: '/',
         description: 'Learn about Spotted Apple and our music sharing approach.'
-    },
-    { 
+      },
+      { 
+        key: 'about',
         name: 'About',
         href: '/',
         description: 'Explore Spotted Apple and our mission.'
@@ -30,9 +34,10 @@ const links = [
       <>
         {links.map((link) => {
           return (
-            <h2 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <h2 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                key={link.key}
+            >
                 <Link
-                key={link.name}
                 href={link.href}
                 >
                 <p className="mb-3 text-2xl font-semibold">{link.name}
