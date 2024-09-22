a079673315091c0c2b11258b9444802e146d0bd3
Zachary J McCann <zackjmccan@gmail.com>
2024-09-22 13:05:49 -0400

    refactor data model

--- backend/primary/bin/init.sql
+++ backend/primary/bin/init.sql
@@ -1,19 +1,16 @@
 CREATE TABLE IF NOT EXISTS users (
-  user_id  BIGSERIAL PRIMARY KEY,
-  email    TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL,
-  created  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  user_id    BIGSERIAL PRIMARY KEY,
+  email      TEXT UNIQUE NOT NULL,
+  first_name VARCHAR,
+  last_name  VARCHAR,
+  created    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  modified   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS access_tokens (
-  access_token_id VARCHAR PRIMARY KEY,
-  account         VARCHAR,
-  user_id         BIGINT references users(user_id),
-  access_token    VARCHAR,
-  token_type      VARCHAR,
-  scope           VARCHAR,
-  expiration_time TIMESTAMPTZ NOT NULL,
-  refresh_token   VARCHAR,
-  created         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  modified        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS user_authentication (
+  auth_id  BIGSERIAL PRIMARY KEY,
+  user_id  BIGINT references users(user_id),
+  password VARCHAR,
+  created  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
