450e87f3f5387939a8e6d07f2563613fbadf77f2
Zachary J McCann <zackjmccan@gmail.com>
2024-09-26 19:35:19 -0400

    adding UserProvider component

--- frontend/spotted-apple/app/layout.tsx
+++ frontend/spotted-apple/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from "@/app/ui/fonts";
 import type { Metadata } from "next";
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export const metadata: Metadata = {
   title: "Spotted Apple",
@@ -14,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <UserProvider>
       <body className={inter.className}>{children}</body>
+    </UserProvider>
     </html>
   );
 }
