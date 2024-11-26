cfbd83d4cae1b216edc7ca8ac85c03394f4a8884
Zachary J McCann <zackjmccan@gmail.com>
2024-11-26 14:28:03 -0500

    changing servername var to flask default

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,4 +4,4 @@ from distutils.util import strtobool
 
 FLASK_DEBUG = bool(strtobool(os.getenv('FLASK_DEBUG', 'false')))
 SECRET_KEY = os.getenv('SECRET_KEY')
-SERVER_NAME = os.getenv('SERVER_NAME', f'localhost:{os.getenv("PORT", "8000")}')
+SERVER_NAME = os.getenv('SERVER_NAME')
