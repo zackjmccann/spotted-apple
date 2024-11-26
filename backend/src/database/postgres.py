c2e9dce3b7047d583cf987b19c41bbb299df71c5
Zachary J McCann <zackjmccan@gmail.com>
2024-11-26 14:29:32 -0500

    adding pg abstraction

--- backend/src/database/postgres.py
+++ backend/src/database/postgres.py
@@ -6,12 +6,14 @@ import psycopg2
 from psycopg2 import Error
 
 class Postgres:
-    def __init__(self, service, namespace, database):
-        self.client_username = os.getenv('CLIENTUSERNAME')
-        self.client_password = os.getenv('CLIENTPASSWORD')
-        self.service = service 
-        self.namespace = namespace # 'backend'
-        self.database = database # 'sa-staging'
+    def __init__(self):
+        self.cluster = os.getenv('CLUSTER_NAME')
+        self.client_username = os.getenv('CLIENT_USERNAME')
+        self.client_password = os.getenv('CLIENT_PASSWORD')
+        self.service_host = os.getenv(f'{self.cluster}_RW_SERVICE_HOST')
+        self.service_port = os.getenv(f'{self.cluster}_RW_SERVICE_PORT')
+        self.database = os.getenv(f'POSTGRES_DB')
+        self.service = f'{self.service_host}:{self.service_port}'
         self.connection_string = self._get_connection_string()
 
     def get_connection(self):
@@ -19,7 +21,7 @@ class Postgres:
 
     def _get_connection_string(self):
         return (f'postgresql://{self.client_username}:{self.client_password}'
-                f'@{self.service}.{self.namespace}/{self.database}')
+                f'@{self.service}/{self.database}')
 
     def execute_query(self, query_data: dict):
         with self.get_connection() as conn:
