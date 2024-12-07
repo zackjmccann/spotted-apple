b88f6f8fb1ebe514ac937d826a242b760f054d68
Zachary J McCann <zackjmccan@gmail.com>
2024-12-07 11:45:08 -0500

    adding data access to backend server

--- frontend/spotted-apple/app/features/create-account-form-feature/actions.tsx
+++ frontend/spotted-apple/app/features/create-account-form-feature/actions.tsx
@@ -25,5 +25,31 @@ export async function createAccount(prevState: SignUpState, formData: FormData,)
             formData: enterValues};
     };
 
+    // Check user email
+    const email: string = enterValues.email
+
+    // try {
+    //     const response = await fetch(
+    //         'http://localhost:3000/api/check-email',
+    //     {
+    //         method: 'POST',
+    //         headers: { 'Content-Type': 'application/json' },
+    //         body: JSON.stringify({ email }),
+    //     });
+  
+    //     const data = await response.json();
+  
+    //     if (data.exists) {
+    //       alert('Email already exists!');
+    //       setEmailExists(true);
+    //     } else {
+    //       setEmailExists(false);
+    //       setShowPasswordInput(true);
+    //     }
+    //   } catch (error) {
+    //     console.error('Error:', error);
+    //     alert('Something went wrong');
+    //   }
+
     return { created: true, passwordSet: false, formData: enterValues, } as SignUpState;
 };
