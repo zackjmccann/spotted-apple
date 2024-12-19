054ea8228fd91f4712c7d26bd21cf252750e1503
Zachary J McCann <zackjmccan@gmail.com>
2024-12-18 23:10:49 -0500

    removing comments

--- backend/operations/app/services/api.py
+++ backend/operations/app/services/api.py
@@ -17,8 +17,8 @@ def email_is_registered(email: str, headers: dict) -> bool:
             '/register/introspect',
             {'email': email},
             {
-                'Content-Type': headers.get('Content-Type'), # 'application/json'
-                'Authorization': headers.get('Authorization'), # 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzcG90dGVkLWFwcGxlLWJhY2tlbmQiLCJhdWQiOlsiMSJdLCJpYXQiOjE3MzQ1MDExODksImV4cCI6MTczNDUwMTc4OSwianRpIjoiYmFja2VuZF9zZXJ2aWNlc18yMDI0MTIxOF8wMDUzIiwiY29udGV4dCI6eyJ1c2VybmFtZSI6ImZyb250ZW5kIiwicm9sZXMiOlsiY2xpZW50Il19fQ.4iLJgRy2bSEbVCyTogs5kF3rwzcyJpXVFtKQqszf5cY'
+                'Content-Type': headers.get('Content-Type'),
+                'Authorization': headers.get('Authorization'),
             }) 
    
         if response.status_code == 200:
