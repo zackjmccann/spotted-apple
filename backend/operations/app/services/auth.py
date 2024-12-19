82940484c583978f6509e9541c6d33d2a595521d
Zachary J McCann <zackjmccan@gmail.com>
2024-12-18 23:05:10 -0500

    adding auth service for mw

--- backend/operations/app/services/auth.py
+++ backend/operations/app/services/auth.py
@@ -14,41 +14,22 @@ if None in [SECRET_KEY, ALOGRITHMS, APP_ID]:
     raise AuthenticationError('An authentication failure occurred')
 
 def authenticate_with_auth_server(request):
-    # TODO: Refactor route to call to the auth endpoint
     access_token = None
     access_token = request.headers['Authorization'].split(" ")[1]
 
     if is_valid_token(access_token):
-        return {'valid': True, 'code': 200}
-    
-    data = request.get_json()
-    response = refresh_tokens(data.get('refresh_token', None))
-    
-    if response['tokens_refreshed']:
-        return {'valid': True, 'code': 200}
-
-    return {
-        'code': 400,
-        'valid': False,
-        'status': response['status'],
-        'message': response['message']
-    }
+        return {'code': 200, 'valid': True}
+    else:
+        return {'code': 401, 'valid': False}
 
 def is_valid_token(access_token):
-    response = call_auth_server('introspect', {'access_token': access_token})
-    data = response.json()
-    return data['valid']
+    response = call_auth_server('/auth/introspect', {'access_token': access_token})
+    if response.status_code == 401:
+        return False
+    else:
+        return True
 
-def refresh_tokens(refresh_token):
-    response = call_auth_server('refresh', {'refresh_token': refresh_token})
-    if response['status_code'] == 200:
-        return {'tokens_refreshed': True}
-    
-    data = response.get_json()
-    data.update({'tokens_refreshed': False})
-    return data
-
-def call_auth_server(endpoint, payload):
+def call_auth_server(endpoint, payload, headers = None):
     AUTH_SERVICE = os.getenv('AUTH_SERVICE')
-    response = requests.post(f'http://{AUTH_SERVICE}/auth/{endpoint}', json=payload)
+    response = requests.post(f'http://{AUTH_SERVICE}{endpoint}', json=payload, headers=headers)
     return response
