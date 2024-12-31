c3581788593d54e09087c6399206f01bd42aad5a
Zachary J McCann <zackjmccan@gmail.com>
2024-12-31 13:31:55 -0500

    refactor auth module imports

--- frontend/spotted-apple/features/login/actions.tsx
+++ frontend/spotted-apple/features/login/actions.tsx
@@ -1,5 +1,9 @@
+"use server";
+
 import { LoginFormSchema } from '@/features/login/schemas'
 import { LoginState } from '@/features/login/types'
+import { authFetch, RequestParameters } from "@/data-access/auth";
+
 
 function validateFields(formData: FormData) {
     return LoginFormSchema.safeParse({
@@ -10,16 +14,40 @@ function validateFields(formData: FormData) {
 
 export async function login(prevState: LoginState, formData: FormData,) {
     const validatedFields = validateFields(formData);
-    const enterValues: { [key: string]: string } = {};
-    formData.forEach((value, key) => { enterValues[key] = value.toString(); });
+    const enteredValues: { [key: string]: string } = {};
+    formData.forEach((value, key) => { enteredValues[key] = value.toString(); });
 
     if (!validatedFields.success) {
         const formErrors = validatedFields.error.flatten().fieldErrors;
-        return { errors: formErrors, formData: enterValues};
+        return { errors: formErrors, formData: enteredValues};
     };
     
     // Login logic
-    // 
-    
+    const { email, password } = enteredValues;
+    const authRequest: RequestParameters = {
+        method: 'GET',
+        endpoint: '/auth/authorize',
+        queryParameters: {
+            email: email,
+            password: password,
+            grant_type: 'authorization',
+        },
+    }
+
+    try {
+        const response = await authFetch(authRequest);    
+        const data = await response.json()
+        
+        if (!response.ok) {
+            throw new Error(`${data.message}`)
+        } else {
+            console.log('Authentication request successful.')
+            // return data
+            console.log(`data: ${JSON.stringify(data)}`)
+        }
+    } catch (error) {
+        const err = error instanceof Error ? error.message : String(error)
+        console.log(`Authentication Request Failed: ${err}`);
+    }
     return prevState;
 };
